6d68b81c53f91f142dc3965f749788e72d993b85
filiptodosovski <filiptodosovski@gmail.com>
2024-07-13 23:30:57 +0200

    feat: implement react-query

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,20 +2,23 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import '@/styles/global.css'
 import { SessionProvider } from 'next-auth/react'
+import { RootProvider } from '@/providers/RootProvider'
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
     <SessionProvider session={session}>
-      <>
-        <Head>
-          <title>Chat App</title>
-        </Head>
-        <div className="bg-bgPrimary min-h-screen">
-          <main>
-            <Component {...pageProps} />
-          </main>
-        </div>
-      </>
+      <RootProvider session={session}>
+        <>
+          <Head>
+            <title>Chat App</title>
+          </Head>
+          <div className="bg-bgPrimary min-h-screen">
+            <main>
+              <Component {...pageProps} />
+            </main>
+          </div>
+        </>
+      </RootProvider>
     </SessionProvider>
   )
 }
--- apps/web/src/services/auth/auth.ts
+++
@@ -1,9 +0,0 @@
-import { createApiHandler } from '@/services/utils'
-import { TSignInValidationSchema } from '@/domains/auth/components/SignInForm/validation/schema'
-import axios from 'axios'
-
-export const login = createApiHandler(
-  async (credentials: TSignInValidationSchema) =>
-    await axios.post(`/auth/login`, credentials),
-  ['login'],
-)
