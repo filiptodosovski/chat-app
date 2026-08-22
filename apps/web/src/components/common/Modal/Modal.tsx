7070ee70ae01c484a04a21c896871fc14ea44846
filiptodosovski <filiptodosovski@gmail.com>
2026-08-22 09:37:56 +0200

    fix: vulnerability

--- apps/web/src/components/common/Modal/Modal.tsx
+++ apps/web/src/components/common/Modal/Modal.tsx
@@ -4,7 +4,7 @@ import { SignInForm } from '@/domains/auth/components/SignInForm/SignInForm'
 
 export const Modal = ({ open, onClose }: IModalProps) => {
   return (
-    <Dialog open={open} onClose={onClose} className="relative z-50">
+    <Dialog open={open} onClose={onClose} className="relative z-55">
       <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
         <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
           <SignInForm />
